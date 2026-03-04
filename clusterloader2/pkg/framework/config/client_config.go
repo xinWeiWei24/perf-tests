593775649b7792fd422a37b66b8dcd58fefb12f0
Xinwei Liu <nadialiuwei@gmail.com>
2026-03-04 13:31:20 +1100

    Hyperscale benchmarking in virtual nodes

--- clusterloader2/pkg/framework/config/client_config.go
+++ clusterloader2/pkg/framework/config/client_config.go
@@ -31,8 +31,8 @@ import (
 
 const (
 	contentType = "application/vnd.kubernetes.protobuf"
-	qps         = 100
-	burst       = 200
+	qps         = 500
+	burst       = 1000
 )
 
 // PrepareConfig creates and initializes client config.
